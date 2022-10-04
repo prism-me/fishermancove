a565a6469af7af331a0901c9add29b37410224f7
faiza-zakir <faiza.prism@gmail.com>
2022-10-04 16:38:52 +0500

    add oct pop up

--- src/components/popup/PopUp.jsx
+++ src/components/popup/PopUp.jsx
@@ -1,50 +1,43 @@
 import React from "react";
 import { Modal, Container } from "react-bootstrap";
-import CancelIcon from '@material-ui/icons/Cancel';
-import popupimg from "../../assets/img/FCR-July-Seychelles.jpg";
-// import LazyLoad from 'react-lazyload';
+import CancelIcon from "@material-ui/icons/Cancel";
+import popupimg from "../../assets/img/OctoberCampaign_post.jpg";
 
 const PopUp = (props) => {
-
-
-    return (
-        <Modal
-            {...props}
-            size="md"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-            className="Documents"
-            style={{ paddingLeft: "0px" }}
-        >
-            <span className={"modalIcon"} onClick={props.onHide}>
-
-                <CancelIcon class="icon" />
-            </span>
-            <Container>
-                <div>
-
-                    <Modal.Body>
-                        <a href={`/${props.activeLang}/offers/10-percentage-off-dinner-for-two`} >
-                            {/* <LazyLoad height={200} > */}
-                            <img
-                                src={popupimg}
-                                alt="popup fishermans cove resort"
-                                className="img-fluid"
-                            // style={{
-                            //     width: "100%",
-                            //     maxWidth: "600px",
-                            //     height: "auto"
-                            // }}
-                            />
-                            {/* </LazyLoad> */}
-                        </a>
-                    </Modal.Body>
-                </div>
-            </Container>
-        </Modal>
-    );
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      className="Documents"
+      style={{ paddingLeft: "0px" }}
+    >
+      <span className={"modalIcon"} onClick={props.onHide}>
+        <CancelIcon class="icon" />
+      </span>
+      <Container>
+        <div>
+          <Modal.Body>
+            <a
+              href={`/${props.activeLang}/offers/10-percentage-off-dinner-for-two`}
+            >
+              <img
+                src={popupimg}
+                alt="popup fishermans cove resort"
+                className="img-fluid"
+                // style={{
+                //     width: "100%",
+                //     maxWidth: "600px",
+                //     height: "auto"
+                // }}
+              />
+            </a>
+          </Modal.Body>
+        </div>
+      </Container>
+    </Modal>
+  );
 };
-
-
 
 export default PopUp;
