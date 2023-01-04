789650ea9237cb303d6c0509c2151cf4bc64a35c
faiza-zakir <faiza.prism@gmail.com>
2023-01-04 13:02:12 +0500

    merge productioncode

--- src/components/pages/AboutSeychelles.js
+++ src/components/pages/AboutSeychelles.js
@@ -1,22 +1,25 @@
-import React, { useEffect } from 'react'
-import SeychellesIntroBlock from '../sections/about-seychelles/intro-block';
-import SeychellesPillarsBlock from '../sections/about-seychelles/pillars-block';
+import React, { useEffect } from "react";
+import SeychellesIntroBlock from "../sections/about-seychelles/intro-block";
+import SeychellesPillarsBlock from "../sections/about-seychelles/pillars-block";
 import SEOTags from "../sections/common/SEOTags";
 import PageLayout from "../layouts/PageLayout";
-import { constants } from '../../utils/constants';
+import { constants } from "../../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchSections, removeSelectedSection, } from '../../redux/actions/sectionsActions';
+import {
+  fetchSections,
+  removeSelectedSection,
+} from "../../redux/actions/sectionsActions";
 
 const AboutSeychelles = (props) => {
-
   const dispatch = useDispatch();
-  const activeLang = localStorage.getItem('lang');
+  const activeLang = localStorage.getItem("lang");
 
   //About seychelles page
   const pageId = `629707a57f20d344c2209512`;
 
   useEffect(() => {
-    if (activeLang && activeLang !== "" && pageId && pageId !== "") dispatch(fetchSections(pageId, activeLang));
+    if (activeLang && activeLang !== "" && pageId && pageId !== "")
+      dispatch(fetchSections(pageId, activeLang));
     return () => {
       dispatch(removeSelectedSection());
     };
@@ -29,47 +32,49 @@ const AboutSeychelles = (props) => {
   const breadcrumbItems = [
     {
       text: `${constants?.site_content?.about_page?.bread_crumb?.title[activeLang]}`,
-      link: '/',
+      link: "/",
       isActive: false,
     },
     {
       text: `${constants?.site_content?.aboutSeychelles_page?.bread_crumb?.title2[activeLang]}`,
-      link: '/about-seychelles',
+      link: "/about-seychelles",
       isActive: true,
     },
-  ]
+  ];
   return (
     <div className="bg-white seychelles-wrapper">
       <SEOTags meta={meta} />
 
-      {
-        banner ?
-          <PageLayout
-            header={{ isMobile: props.isMobile, isTop: props.isTop }}
-            banner={{ title: banner?.section_name, image: banner?.section_avatar?.avatar }}
-            breadCrumb={{ items: breadcrumbItems }}
-            activeLang={activeLang}
-          >
-            {/*/!*====== INTRO START ======*!/*/}
-            <SeychellesIntroBlock data={intro}
-              activeLang={activeLang}
-            />
-            {/*====== INTRO END ======*/}
-            {/*====== PILLARS START ======*/}
-            <SeychellesPillarsBlock
-              activeLang={activeLang}
-            />
-          </PageLayout>
-          :
-          <div className={"preloader align-items-center justify-content-center"}>
-            <div className="cssload-container">
-              <div className="cssload-loading"><i /><i /><i /><i /></div>
+      {banner ? (
+        <PageLayout
+          header={{ isMobile: props.isMobile, isTop: props.isTop }}
+          banner={{
+            title: banner?.section_name,
+            image: banner?.section_avatar?.avatar,
+          }}
+          breadCrumb={{ items: breadcrumbItems }}
+          activeLang={activeLang}
+        >
+          {/*/!*====== INTRO START ======*!/*/}
+          <SeychellesIntroBlock data={intro} activeLang={activeLang} />
+          {/*====== INTRO END ======*/}
+          {/*====== PILLARS START ======*/}
+          <SeychellesPillarsBlock activeLang={activeLang} />
+        </PageLayout>
+      ) : (
+        <div className={"preloader align-items-center justify-content-center"}>
+          <div className="cssload-container">
+            <div className="cssload-loading">
+              <i />
+              <i />
+              <i />
+              <i />
             </div>
           </div>
-
-      }
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default AboutSeychelles;
--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import Blogsidebar from '../layouts/Blogsidebar';
-import { Link } from 'react-router-dom';
-import API from '../../langapi/http';
+import React, { useEffect, useState } from "react";
+import Blogsidebar from "../layouts/Blogsidebar";
+import { Link } from "react-router-dom";
+import API from "../../langapi/http";
 import ReactPaginate from "react-paginate";
-import PageLayout from '../layouts/PageLayout';
-import { constants } from '../../utils/constants';
+import PageLayout from "../layouts/PageLayout";
+import { constants } from "../../utils/constants";
 import SEOTags from "../sections/common/SEOTags";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchSections, removeSelectedSection } from '../../redux/actions/sectionsActions';
+import {
+  fetchSections,
+  removeSelectedSection,
+} from "../../redux/actions/sectionsActions";
 
 const Blog = (props) => {
-
   const dispatch = useDispatch();
-  const activeLang = localStorage.getItem('lang');
+  const activeLang = localStorage.getItem("lang");
 
   //blog page
-  const pageId = '629a0927f08f18452f364ba2';
+  const pageId = "629a0927f08f18452f364ba2";
 
   useEffect(() => {
-    if (activeLang && activeLang !== "" && pageId && pageId !== "") dispatch(fetchSections(pageId, activeLang));
+    if (activeLang && activeLang !== "" && pageId && pageId !== "")
+      dispatch(fetchSections(pageId, activeLang));
     return () => {
       dispatch(removeSelectedSection());
     };
@@ -32,37 +35,37 @@ const Blog = (props) => {
   const [blogData, setBlogData] = useState([]);
 
   const getBlogData = () => {
-    API.get(`/blogs?lang=${activeLang}`).then(response => {
-      const allData = response.data?.data;
-      setBlogData(allData);
-    })
-      .catch(err => {
-        console.log(err)
+    API.get(`/blogs?lang=${activeLang}`)
+      .then((response) => {
+        const allData = response.data?.data;
+        setBlogData(allData);
       })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   // recentBlog
 
   const [recentBlog, setRecentBlog] = useState([]);
 
   const getRecentData = () => {
-    API.get(`/blogs?lang=${activeLang}`).then(response => {
-      const recentData = response.data?.data.sort(
-        (a, b) =>
-          new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
-      )
-      setRecentBlog(recentData);
-    })
-      .catch(err => {
-        console.log(err)
+    API.get(`/blogs?lang=${activeLang}`)
+      .then((response) => {
+        const recentData = response.data?.data.sort(
+          (a, b) =>
+            new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+        );
+        setRecentBlog(recentData);
       })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
-
     getBlogData();
     getRecentData();
-
   }, [activeLang]);
 
   // pagination code
@@ -82,22 +85,21 @@ const Blog = (props) => {
           </div>
           <div className="post-desc">
             <h2>
-              <Link to={`/${activeLang}/blogs/${x?.slug}`}>
-                {x?.title}
-              </Link>
+              <Link to={`/${activeLang}/blogs/${x?.slug}`}>{x?.title}</Link>
             </h2>
             <ul className="post-meta">
-
-              <li><Link to="#"><i className="far fa-calendar-alt" />
-                {new Date(x?.created_at).toLocaleDateString()}
-              </Link></li>
+              <li>
+                <Link to="#">
+                  <i className="far fa-calendar-alt" />
+                  {new Date(x?.created_at).toLocaleDateString()}
+                </Link>
+              </li>
             </ul>
             <p
               dangerouslySetInnerHTML={{
-                __html: x?.short_description?.substr(0, 200)
+                __html: x?.short_description?.substr(0, 200),
               }}
-            >
-            </p>
+            ></p>
             <div className="post-footer">
               <div className="author">
                 <Link to="#">
@@ -106,8 +108,13 @@ const Blog = (props) => {
                 </Link>
               </div>
               <div className="read-more">
-                <Link to={`/${activeLang}/blogs/${x?.slug}`}><i className="far fa-arrow-right" />
-                  {constants?.site_content?.blog_page?.blog_grid?.btn_text[activeLang]}
+                <Link to={`/${activeLang}/blogs/${x?.slug}`}>
+                  <i className="far fa-arrow-right" />
+                  {
+                    constants?.site_content?.blog_page?.blog_grid?.btn_text[
+                      activeLang
+                    ]
+                  }
                 </Link>
               </div>
             </div>
@@ -125,77 +132,74 @@ const Blog = (props) => {
   const breadcrumbItems = [
     {
       text: `${constants?.site_content?.about_page?.bread_crumb?.title[activeLang]}`,
-      link: '/',
+      link: "/",
       isActive: false,
     },
     {
       text: `${constants?.site_content?.blog_page?.bread_crumb?.title2[activeLang]}`,
-      link: '/blog',
+      link: "/blog",
       isActive: true,
     },
   ];
-
 
   return (
     <div>
       <SEOTags meta={meta} />
 
-      {
-        blogData.length > 0 ?
-          <PageLayout
-            header={{ isMobile: props.isMobile, isTop: props.isTop }}
-            banner={{ title: banner?.section_name, image: banner?.section_avatar?.avatar }}
-            breadCrumb={{ items: breadcrumbItems }}
-            activeLang={activeLang}
-          >
-            {/*====== BLOG SECTION START ======*/}
-            <section className="blog-section pt-40 pb-100">
-              <div className="container">
-                <div className="row justify-content-center">
-                  <div className="col-lg-8 col-md-10">
-                    <div className="blog-loop">
+      {blogData.length > 0 ? (
+        <PageLayout
+          header={{ isMobile: props.isMobile, isTop: props.isTop }}
+          banner={{
+            title: banner?.section_name,
+            image: banner?.section_avatar?.avatar,
+          }}
+          breadCrumb={{ items: breadcrumbItems }}
+          activeLang={activeLang}
+        >
+          {/*====== BLOG SECTION START ======*/}
+          <section className="blog-section pt-40 pb-100">
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-lg-8 col-md-10">
+                  <div className="blog-loop">{displayBlogs}</div>
 
-                      {displayBlogs}
-
-                    </div>
-
-
-                    <ReactPaginate
-                      previousLabel={<i className="far fa-angle-double-left" />}
-                      nextLabel={<i className="far fa-angle-double-right" />}
-                      pageCount={pageCount}
-                      onPageChange={changePage}
-                      containerClassName={"paginationBttns"}
-                      previousLinkClassName={"previousBttn"}
-                      nextLinkClassName={"nextBttn"}
-                      disabledClassName={"paginationDisabled"}
-                      activeClassName={"paginationActive"}
-                    />
-
-                  </div>
-                  {/* Blog Sidebar */}
-                  <div className="col-lg-4 col-md-8 col-sm-10">
-                    <Blogsidebar
-                      recentBlog={recentBlog}
-                      activeLang={activeLang}
-                    />
-                  </div>
+                  <ReactPaginate
+                    previousLabel={<i className="far fa-angle-double-left" />}
+                    nextLabel={<i className="far fa-angle-double-right" />}
+                    pageCount={pageCount}
+                    onPageChange={changePage}
+                    containerClassName={"paginationBttns"}
+                    previousLinkClassName={"previousBttn"}
+                    nextLinkClassName={"nextBttn"}
+                    disabledClassName={"paginationDisabled"}
+                    activeClassName={"paginationActive"}
+                  />
+                </div>
+                {/* Blog Sidebar */}
+                <div className="col-lg-4 col-md-8 col-sm-10">
+                  <Blogsidebar
+                    recentBlog={recentBlog}
+                    activeLang={activeLang}
+                  />
                 </div>
               </div>
-            </section>
-
-          </PageLayout>
-          :
-          <div className={"preloader align-items-center justify-content-center"}>
-            <div className="cssload-container">
-              <div className="cssload-loading"><i /><i /><i /><i /></div>
+            </div>
+          </section>
+        </PageLayout>
+      ) : (
+        <div className={"preloader align-items-center justify-content-center"}>
+          <div className="cssload-container">
+            <div className="cssload-loading">
+              <i />
+              <i />
+              <i />
+              <i />
             </div>
           </div>
-
-      }
+        </div>
+      )}
     </div>
-
   );
-}
+};
 
 export default Blog;
